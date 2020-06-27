5c0cf6e9d64ad8d5c2b5c93c0bf46f5b79abc400
Jordan Dube <32602604+JordanDube@users.noreply.github.com>
2020-06-27 10:35:19 -0400

    All scenes and jumping
    All scenes grayboxed and jumping corrected so it doesn't double jump

--- Celebration eJam/Assets/Scripts/GameManager.cs
+++ Celebration eJam/Assets/Scripts/GameManager.cs
@@ -13,6 +13,14 @@ public class GameManager : MonoBehaviour
     public bool balloon = false;
     public bool partyPopper = false;
 
+    [SerializeField] int[] cameraX;
+    [SerializeField] GameObject mainCam;
+    Player player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Cat").gameObject.GetComponent<Player>();
+    }
 
     public void GetItem(string item)
     {
@@ -36,6 +44,51 @@ public class GameManager : MonoBehaviour
             case "PartyPopper": //check off hat
                 break;
         }
+    }
+
+    public void SwitchScene(int toLocation, int fromLocation)
+    {
+        switch(toLocation)
+        {
+            case 0:
+                switch (fromLocation)
+                {
+                    case 1: //cat's location from the kitchen
+                        player.gameObject.transform.position = new Vector3(7.27f, -3.21f, 0f);
+                        break;
+                    case 2: //cat's location from coming dow nthe stairs
+                        player.gameObject.transform.position = new Vector3(-7.05f, -3.21f, 0f);
+                        break;
+                }
+                break;
+            case 1: //cat's location from livingroom
+                player.gameObject.transform.position = new Vector3(37f, -3.21f, 0f);
+                break;
+            case 2: switch(fromLocation)
+            {
+                case 0: //cat's location from coming up the stairs
+                        player.gameObject.transform.position = new Vector3(60.5f, -3.21f, 0f);
+                        break;
+                case 3: //cat's location from the bedroom
+                        player.gameObject.transform.position = new Vector3(53.8f, -3.21f, 0f);
+                        break;
+                case 4: //cat's location from the bathroom
+                        player.gameObject.transform.position = new Vector3(66.8f, -3.21f, 0f);
+                        break;
+            }
+                break;
+            case 3: //cat's location form the hallway
+                player.gameObject.transform.position = new Vector3(95.75f, -3.21f, 0f);
+                break;
+            case 4: //cat's location from hallway
+                player.gameObject.transform.position = new Vector3(114.23f, -3.21f, 0f);
+                break;
+
+        }
+        player.canTravel = false;
+        player.travel = false;
+        mainCam.transform.position = new Vector3(cameraX[toLocation], 1, -10);
+
     }
     
 }
