0052ada5e2c9ad72de2ec559149eaf03025c4d0c
Jordan Dube <32602604+JordanDube@users.noreply.github.com>
2020-06-27 19:53:51 -0400

    Start, end, and hover text

--- Celebration eJam/Assets/Scripts/Player.cs
+++ Celebration eJam/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Scripting.APIUpdating;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -37,6 +38,7 @@ public class Player : MonoBehaviour
     AudioSource audioSource;
     public AudioClip[] meow;
 
+    [SerializeField] Text travelText;
 
     private void Awake() {
         _animator = GetComponent<Animator>();
@@ -52,6 +54,8 @@ public class Player : MonoBehaviour
         gameManager = GameObject.Find("GameManager").gameObject.GetComponent<GameManager>();
         areaTracker[0] = true;
         audioSource = gameObject.GetComponent<AudioSource>();
+        travelText.enabled = false;
+        travelText.text = "";
     }
 
     private bool GameIsOver() {
@@ -177,6 +181,9 @@ public class Player : MonoBehaviour
         
         if (collision.gameObject.tag == "Door")
         {
+            travelText.enabled = true;
+            travelText.text = collision.gameObject.name;
+
             if (travel)
             {
                 canTravel = false;
@@ -228,6 +235,8 @@ public class Player : MonoBehaviour
         }
         canTravel = false;
         travel = false;
+        travelText.enabled = false;
+        travelText.text = "";
     }
 
     private void OnEnable()
---
+++ Celebration eJam/Assets/Scripts/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] GameObject instructions;
+
+    public void StartButton()
+    {
+        instructions.SetActive(true);
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
