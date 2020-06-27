71e8ada7adf8c07606a6289683206bf62d92b535
Josh Jacobs <joshalexjacobs@gmail.com>
2020-06-27 15:35:07 -0400

    added basic game clock

---
+++ Celebration eJam/Assets/Plugins/CandyCoded/Scripts/ScriptableObjects/GameObjectReference.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
+
+// ReSharper disable RequiredBaseTypesConflict
+
+using UnityEngine;
+
+namespace CandyCoded
+{
+
+    [CreateAssetMenu(fileName = "GameObjectReference", menuName = "CandyCoded/GameObjectReference")]
+    [HelpURL("https://github.com/CandyCoded/CandyCoded/blob/master/Documentation/4.%20ScriptableObject/GameObject.md")]
+    public class GameObjectReference : CustomGenericScriptableObject<GameObject>
+    {
+
+    }
+
+}
---
+++ Celebration eJam/Assets/Plugins/CandyCoded/Scripts/ScriptableObjects/StringReference.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
+
+// ReSharper disable RequiredBaseTypesConflict
+
+using UnityEngine;
+
+namespace CandyCoded
+{
+
+    [CreateAssetMenu(fileName = "StringReference", menuName = "CandyCoded/StringReference")]
+    [HelpURL("https://github.com/CandyCoded/CandyCoded/blob/master/Documentation/4.%20ScriptableObject/String.md")]
+    public class StringReference : CustomGenericScriptableObject<string>
+    {
+
+    }
+
+}
